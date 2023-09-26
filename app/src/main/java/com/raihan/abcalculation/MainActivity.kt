b5cfd3121eec6132f50b6905f5bb3acc24bc252c
Syed Raihanuzzaman <syed15-10568@diu.edu.bd>
2023-09-26 13:01:07 +0600

    Drawer layout added and the layout working perfectly.

--- app/src/main/java/com/raihan/abcalculation/MainActivity.kt
+++ app/src/main/java/com/raihan/abcalculation/MainActivity.kt
@@ -2,11 +2,16 @@ package com.raihan.abcalculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.raihan.abcalculation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawlerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,16 @@ class MainActivity : AppCompatActivity() {
         var navController = findNavController(R.id.fragmentContainerView)
         binding.bottombar.setupWithNavController(navController)
 
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.nav_open,R.string.nav_close)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+           true
+       }else super.onOptionsItemSelected(item)
     }
 }
