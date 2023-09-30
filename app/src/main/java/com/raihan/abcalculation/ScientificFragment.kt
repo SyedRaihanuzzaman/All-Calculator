8e7e768934cc12619df8dc3102c668374e933190
Syed Raihanuzzaman <syed15-10568@diu.edu.bd>
2023-09-30 13:33:13 +0600

    chage in ScientificFragment.kt and fragment_scientific.xml

--- app/src/main/java/com/raihan/abcalculation/ScientificFragment.kt
+++ app/src/main/java/com/raihan/abcalculation/ScientificFragment.kt
@@ -13,8 +13,9 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 
 class ScientificFragment : Fragment() {
-    private lateinit var binding:FragmentScientificBinding
-
+    private lateinit var binding: FragmentScientificBinding
+    private var bracketOpenCount = 0
+    private var bracketCloseCount = 0
 
 
     @SuppressLint("SuspiciousIndentation")
@@ -23,48 +24,24 @@ class ScientificFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentScientificBinding.inflate(layoutInflater, container, false)
-
-          binding.btnAc.setOnClickListener{
-              binding.workingsTV.text = ""
-              binding.resultTV.text = ""
-          }
-        /*binding.btnSin.setOnClickListener {
-            val operand = binding.workingsTV.text.toString()
-            val result = Math.sin(Math.toRadians(operand.toDouble()))
-            binding.workingsTV.text = result.toString()
-        }*/
-
-
-        binding.btn0.setOnClickListener {
-            binding.workingsTV.append("0")
-        }
-        binding.btn1.setOnClickListener {
-            binding.workingsTV.append("1")
-        }
-        binding.btn2.setOnClickListener {
-            binding.workingsTV.append("2")
-        }
-        binding.btn3.setOnClickListener {
-            binding.workingsTV.append("3")
-        }
-        binding.btn4.setOnClickListener {
-            binding.workingsTV.append("4")
-        }
-        binding.btn5.setOnClickListener {
-            binding.workingsTV.append("5")
-        }
-        binding.btn6.setOnClickListener {
-            binding.workingsTV.append("6")
-        }
-        binding.btn7.setOnClickListener {
-            binding.workingsTV.append("7")
-        }
-        binding.btn8.setOnClickListener {
-            binding.workingsTV.append("8")
-        }
-        binding.btn9.setOnClickListener {
-            binding.workingsTV.append("9")
-        }
+        binding.btnAc.setOnClickListener { onClear(it) }
+        binding.btnBack.setOnClickListener { onBksp(it) }
+        binding.btn0.setOnClickListener { onDigit(it) }
+        binding.btn1.setOnClickListener { onDigit(it) }
+        binding.btn2.setOnClickListener { onDigit(it) }
+        binding.btn3.setOnClickListener { onDigit(it) }
+        binding.btn4.setOnClickListener { onDigit(it) }
+        binding.btn5.setOnClickListener { onDigit(it) }
+        binding.btn6.setOnClickListener { onDigit(it) }
+        binding.btn7.setOnClickListener { onDigit(it) }
+        binding.btn8.setOnClickListener { onDigit(it) }
+        binding.btn9.setOnClickListener { onDigit(it) }
+        binding.btnDot.setOnClickListener { decimalPoint(it) }
+        binding.btnAddition.setOnClickListener { onOperator(it) }
+        binding.btnSubstraction.setOnClickListener { onOperator(it) }
+        binding.btnDivision.setOnClickListener { onOperator(it) }
+        binding.btnMultiplication.setOnClickListener { onOperator(it) }
+        binding.btnEqual.setOnClickListener { onEqual(it) }
         binding.btnSin.setOnClickListener {
             binding.workingsTV.append("sin")
         }
@@ -80,71 +57,294 @@ class ScientificFragment : Fragment() {
         binding.btnIn.setOnClickListener {
             binding.workingsTV.append("In")
         }
+        binding.btnFactorial.setOnClickListener {
+            binding.workingsTV.append("!")
+        }
+        binding.btnSquare.setOnClickListener {
+            binding.workingsTV.append("^")
+        }
+        binding.btnRootover.setOnClickListener {
+            binding.workingsTV.append("√")
+        }
+        binding.btn1byx.setOnClickListener {
+            binding.workingsTV.append("1/")
+        }
         binding.btnStartBracket.setOnClickListener {
             binding.workingsTV.append("(")
         }
         binding.btnEndBracket.setOnClickListener {
             binding.workingsTV.append(")")
         }
-        binding.btnDot.setOnClickListener {
-            binding.workingsTV.append(".")
-        }
 
-        binding.btnAddition.setOnClickListener {
-            binding.workingsTV.append("+")
-        }
-        binding.btnSubstraction.setOnClickListener {
-            binding.workingsTV.append("-")
-        }
-        binding.btnDivision.setOnClickListener {
-            binding.workingsTV.append("/")
-        }
-        binding.btnMultiplication.setOnClickListener {
-            binding.workingsTV.append("×")
-        }
-        binding.btnFactorial.setOnClickListener {
-            binding.workingsTV.append("!")
-        }
-
-        binding.btnEqual.setOnClickListener {
-            val expression = ExpressionBuilder(binding.workingsTV.text.toString()).build()
-            val result = expression.evaluate()
-            val longresult = result.toLong()
-
-            if(result==longresult.toDouble()){
-                binding.resultTV.text = longresult.toString()
-
-            }else{
-                binding.resultTV.text = result.toString()
+      /*  fun onBracketOpen(view: View) {
+            if (num && !dot) {
+                binding.workingsTV.append("(")
+                num = false
+                dot = false
+                bracketOpenCount++
             }
         }
+
+        fun onBracketClose(view: View) {
+            if (num && bracketOpenCount > bracketCloseCount) {
+                binding.workingsTV.append(")")
+                num = false
+                dot = false
+                bracketCloseCount++
+            }
+        }*/
+
+
 
 
         return binding.root
     }
 
+    var dot:Boolean = false
+    var num:Boolean = false
 
+    fun onClear(view: View) {
+        binding.workingsTV.text = ""
+        binding.resultTV.text = ""
+
+        num=false
+        dot=false
+    }
+    fun onBksp(view: View) {
+        val string = binding.workingsTV.text.toString()
+        if(string.isNotEmpty()){
+            binding.workingsTV.text = string.substring(0,string.length-1)
+        }
+    }
+    fun onDigit(view: View) {
+        if (binding.workingsTV.text == "0"){
+            binding.workingsTV.text = ""
+        }
+        binding.workingsTV.append((view as Button).text)
+        num= true
+        dot = false
+    }
+    fun decimalPoint(view: View) {
+        if (num && !dot){
+            binding.workingsTV.append(".")
+            dot= true
+            num= false
+        }
+    }
+    fun onOperator(view: View) {
+        if(num && !isoperatorthere(binding.workingsTV.text.toString())){
+            binding.workingsTV.append((view as Button).text)
+            num=false
+            dot=false
+        }
+    }
+
+    private fun isoperatorthere(value: String): Boolean {
+        return if (value.startsWith("-")){
+            false
+        }
+
+        else {
+            value.contains("+") || value.contains("-") || value.contains("*") || value.contains("÷") || value.contains("%")
+        }
+
+    }
+
+    fun onEqual(view: View) {
+        if (num) {
+
+            var value = binding.workingsTV.text.toString()
+            var prefixcheckker = ""
+
+            try { // In try block cuz some people do division by zero....
+
+                if (value.startsWith("-")) {
+                    prefixcheckker = "-"
+                    value = value.substring(1)
+                }
+
+                if (value.contains("-")) {
+                    val splitvalue = value.split("-")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+
+                    if (!prefixcheckker.isEmpty()) {
+                        num1 = prefixcheckker + num1
+                    }
+
+
+
+                    binding.resultTV.text = reducezeroes((num1.toDouble() - num2.toDouble()).toString())
+                }
+
+                else if (value.contains("+")){
+                    val splitvalue = value.split("+")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+
+                    if (!prefixcheckker.isEmpty()) {
+                        num1 = prefixcheckker + num1
+                        binding.resultTV.text = reducezeroes((num1.toDouble() + num2.toDouble()).toString())
+                    }
+
+                    else{
+                        binding.resultTV.text = reducezeroes((num1.toDouble() + num2.toDouble()).toString())
+                    }
+                }
+
+                else if (value.contains("*")){
+                    val splitvalue = value.split("*")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+
+                    if (!prefixcheckker.isEmpty()) {
+                        num1 = prefixcheckker + num1
+                        binding.resultTV.text = reducezeroes((num1.toDouble() * num2.toDouble()).toString())
+                    }
+
+                    else{
+                        binding.resultTV.text = reducezeroes((num1.toDouble() * num2.toDouble()).toString())
+                    }
+                }
+                else if (value.contains("÷")){
+                    val splitvalue = value.split("÷")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+
+                    if (!prefixcheckker.isEmpty()) {
+                        num1 = prefixcheckker + num1
+                        binding.resultTV.text = (num1.toDouble() / num2.toDouble()).toString()
+                    }
+
+                    else{
+                        binding.resultTV.text = (num1.toDouble() / num2.toDouble()).toString()
+                    }
+                }
+
+                else if (value.contains("%")){
+                    val splitvalue = value.split("%")
+                    var num1 = splitvalue[0]
+                    val num2 = splitvalue[1]
+
+                    binding.resultTV.text = ((num1.toDouble() * num2.toDouble())/100).toString()
+                }
+                else if(value.contains("sin")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("sin")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.sin(Math.toRadians(num2.toDouble()))
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("cos")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("cos")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.cos(Math.toRadians(num2.toDouble()))
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("tan")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("tan")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.tan(Math.toRadians(num2.toDouble()))
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("log")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("log")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.log10(num2.toDouble())
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("in")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("in")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.log(num2.toDouble())
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("!")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("!")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    var result = 1.0
+                    for (i in 2..num1.toInt()) {
+                        result *= i.toDouble()
+                    }
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("^")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("^")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = num1.toDouble() * num2.toDouble()
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("√")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("√")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.sqrt(num2.toDouble())
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("√")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("√")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = Math.sqrt(num2.toDouble())
+                    binding.workingsTV.text = result.toString()
+
+                }
+                else if(value.contains("1/")){
+                    binding.workingsTV.append((view as Button).text)
+                    val splitvalue = value.split("1/")
+                    var num1 = splitvalue[0]
+                    var num2 = splitvalue[1]
+                    val result = 1.0 / num2.toDouble()
+                    binding.workingsTV.text = result.toString()
+
+                }
+
+
+
+
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }catch (e: Exception) {
+                e.printStackTrace()
+                binding.resultTV.text = "Error"
+            }
+
+        }
+    }
+
+    private fun reducezeroes(result: String): String {
+        var finalvalue = result
+
+        if (result.contains(".0")){
+            finalvalue = result.substring(0, result.length-2)
+        }
+        return finalvalue
     }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
